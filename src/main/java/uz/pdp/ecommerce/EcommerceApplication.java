caf141f419af18fe122068e91ec420311bf26512
byusufa <jozef12079104293@gmail.com>
2025-08-13 21:29:17 +0500

    EcommerceApplication commit

---
+++ src/main/java/uz/pdp/ecommerce/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package uz.pdp.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableMethodSecurity
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+}
